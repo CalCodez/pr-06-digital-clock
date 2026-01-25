c1e452c9baa3ea7cb8e02b43e388371788837ba5
Calvin <calvincodez@gmail.com>
2026-01-24 21:53:27 -0600

    basic date clock complete

--- js.js
+++ js.js
@@ -14,3 +14,27 @@ const click = 'click';
 const keyup = 'keyup';
 const flexActive = 'flex-active';
 const flexInactive = 'flex-inactive';
+
+const dateDisplay = getById('dateDisplay');
+const hourDisplay = getById('hourDisplay');
+const minuteDisplay = getById('minuteDisplay');
+const secondsDisplay = getById('secondsDisplay');
+
+function showDate(display) {
+	const today = new Date();
+	const region = 'en-US';
+	const options = { weekday: 'long', year: 'numeric', month: 'long', day: '2-digit' };
+	textContent(display, today.toLocaleDateString(region, options));
+}
+showDate(dateDisplay);
+
+function showTime(h, m, s) {
+	setInterval(() => {
+		const time = new Date();
+		textContent(h, time.getHours());
+		textContent(m, time.getMinutes());
+		textContent(s, time.getSeconds());
+		// console.log(time.getHours());
+	}, 1000);
+}
+showTime(hourDisplay, minuteDisplay, secondsDisplay);
